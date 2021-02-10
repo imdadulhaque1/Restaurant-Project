ff02d44f0bb87d1e7fe8e06e629274974f4c6daa
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-02-11 00:55:38 +0600

    Preparing Project for Router

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Menu from './Menu';
+import Home from './Home'
+import About from './About';
+import Contact from './Contact';
 
 const Body = () =>{
       return(
