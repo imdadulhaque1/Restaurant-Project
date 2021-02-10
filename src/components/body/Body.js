b8ddf2ac1dc30c3435dfefe2fa738904e830b183
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-02-11 02:55:05 +0600

    Restaurants Projects Routing Completed

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,11 +3,15 @@ import Menu from './Menu';
 import Home from './Home'
 import About from './About';
 import Contact from './Contact';
+import { Route, Redirect } from 'react-router-dom';
 
 const Body = () =>{
       return(
             <div>
-                  <Menu />
+                  <Route path="/" exact component={Home} />
+                  <Route path="/menu" exact component={Menu} />
+                  <Route path="/contact" exact component={Contact} />
+                  <Route path="/about" exact component={About} />
             </div>
       );
 }
--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import DISHES from '../../data/dishes';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
+import {CardColumns, Modal, ModalBody, ModalFooter, Button } from "reactstrap";
 
 class Menu extends Component{
       state = {
             dishes: DISHES,
-            selectedDish: null
+            selectedDish: null,
+            modalOpen: false
       }
       onDishSelect = dish =>{
-            this.setState({selectedDish: dish});
+            this.setState({
+                  selectedDish: dish,
+                  modalOpen: !this.state.modalOpen
+            });
       }
 
+      toggleModal = ()=>{
+            this.setState({
+                  modalOpen: !this.state.modalOpen
+            })
+      }
       render() {
             const menu = this.state.dishes.map(item =>{
                   return(
@@ -30,12 +40,19 @@ class Menu extends Component{
             return(
                   <div className="container">
                         <div className="row">
-                              <div className="col-6">
+                              <CardColumns>
                                     {menu}
-                              </div>
-                              <div className="col-6">
-                                    {dishDetail}
-                              </div>
+                              </CardColumns>
+                              <Modal isOpen={this.state.modalOpen} onClick={this.toggleModal}>
+                                    <ModalBody>
+                                          {dishDetail}
+                                    </ModalBody>
+                                    <ModalFooter>
+                                          <Button color="secondary" onClick={this.toggleModal}>
+                                                Close
+                                          </Button>
+                                    </ModalFooter>
+                              </Modal>
                         </div>
                   </div>
             )
