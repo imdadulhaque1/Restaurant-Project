44cebc533803a31fa9db69a7fdc7dfb3ec295c66
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-02-21 02:58:48 +0600

    Access the redux store from components

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state =>{
+          console.log("Map state to props: ",state);
+          return{
+                    dishes: state.dishes
+          }
+}
 
 class Home extends Component{
-          
+          componentDidMount(){
+                    console.log("Home State: ",this.state);
+                    console.log("Home props: ",this.props)
+          }
           render() {
                     document.title ="Restaurant";
                     return(
@@ -12,4 +23,4 @@ class Home extends Component{
           }
 }
 
-export default Home;
+export default connect(mapStateToProps) (Home);
--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import DISHES from '../../data/dishes';
-import COMMENTS from '../../data/comments';
+// import DISHES from '../../data/dishes';
+// import COMMENTS from '../../data/comments';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
 import {CardColumns, Modal, ModalBody, ModalFooter, Button } from "reactstrap";
+import {connect} from 'react-redux';
 
+const mapStateToProps =state =>{
+      return{
+            dishes: state.dishes,
+            comments: state.comments
+      }
+}
 
 class Menu extends Component{
       state = {
-            dishes: DISHES,
-            comments: COMMENTS,
+            // dishes: DISHES,
+            // comments: COMMENTS,
             selectedDish: null,
             modalOpen: false
       }
@@ -27,7 +34,7 @@ class Menu extends Component{
       }
       render() {
             document.title="Menu";
-            const menu = this.state.dishes.map(item =>{
+            const menu = this.props.dishes.map(item =>{
                   return(
                         <MenuItem 
                               dish={item} 
@@ -39,7 +46,7 @@ class Menu extends Component{
 
             let dishDetail = null;
             if(this.state.selectedDish != null){
-                  const comments = this.state.comments.filter(comment => comment.dishId === this.state.selectedDish.id
+                  const comments = this.props.comments.filter(comment => comment.dishId === this.state.selectedDish.id
                   )
                   dishDetail = < DishDetail dish={this.state.selectedDish} comments={comments} />
             }
@@ -65,4 +72,4 @@ class Menu extends Component{
       }
 }
 
-export default Menu;
+export default connect(mapStateToProps) (Menu);
